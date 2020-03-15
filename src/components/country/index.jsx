891464f6a4857c4337e644914d1b89f73449ee48
Alan Campora <alan.campora@gmail.com>
2020-03-15 18:58:58 -0300

    refactor(*): column for country view

--- src/components/country/index.jsx
+++ src/components/country/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import progessionData from './data';
 import Curve from '../graphs/curve';
-import { PageHeader } from 'antd';
+import { PageHeader, Row, Col } from 'antd';
 import { flag } from 'country-emoji';
 
-const Country = ({countryId}) => {
-  console.log({countryId});
+const Country = ({ countryId }) => {
+	console.log({ countryId });
 	const countryData = progessionData[countryId];
 	return (
 		<div>
-			<PageHeader title={`${flag(countryId)} ${countryId}`}/>
-			<Curve rawData={countryData} />
+			<Row justify="center">
+				<Col span={16}>
+					<PageHeader title={`${flag(countryId)} ${countryId}`} />
+				</Col>
+				<Col span={16}>
+					<PageHeader title="Progression of inffected people day by day"/>
+					<Curve rawData={countryData} />
+				</Col>
+			</Row>
 		</div>
 	);
 };
