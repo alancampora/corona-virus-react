772adb8960513bdcb5a89eec5d3e37d67c928b3f
Alan Campora <alan.campora@gmail.com>
2020-03-15 20:49:41 -0300

    refactor(layout): add back button

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import { Layout, PageHeader } from 'antd';
-import { StyledContent } from './styled';
+import {Layout, Button} from 'antd';
+import {LeftOutlined} from '@ant-design/icons';
+import {StyledContent} from './styled';
 import 'antd/dist/antd.css';
-const { Header, Content, Footer } = Layout;
+import {useHistory} from 'react-router-dom'
 
-const BaseLayout = ({ title, children }) => {
-	return (
-		<Layout className="layout" theme="light">
-			<Header ghost={true} style={{ color: 'white', 'font-size': '1.2rem' }}>
-				{title}
-			</Header>
-			<Content style={{ padding: '0 50px' }}>
-				<StyledContent>{children}</StyledContent>
-			</Content>
-			<Footer style={{ textAlign: 'center' }}>
-				Corona Virus Statistics ©2020 Created by Alan Campora
-			</Footer>
-		</Layout>
-	);
+const {Header, Content, Footer} = Layout;
+
+const BaseLayout = ({title, children}) => {
+    const history = useHistory();
+    return (
+        <Layout className="layout" theme="light">
+            <Header ghost={true}
+                    style={{color: 'white', 'font-size': '1.2rem'}}
+            >
+                <div>
+                    <Button shape="circle" icon={<LeftOutlined/>} onClick={() => history.goBack()}
+                            style={{
+                                color: 'white',
+                                background: 'black',
+                                border: '0px',
+                            }}
+                    />
+                    <span>{title} </span>
+                </div>
+            </Header>
+            <Content style={{padding: '0 50px'}}>
+                <StyledContent>{children}</StyledContent>
+            </Content>
+            <Footer style={{textAlign: 'center'}}>
+                Corona Virus Statistics ©2020 Created by Alan Campora
+            </Footer>
+        </Layout>
+    );
 };
 
 export default BaseLayout;
