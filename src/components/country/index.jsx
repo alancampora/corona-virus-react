7b89d89d9f1f1ee1969be8a71255b1aa38eb10af
Alan Campora <alan.campora@gmail.com>
2020-03-15 19:34:46 -0300

    refactor(country): add more info

--- src/components/country/index.jsx
+++ src/components/country/index.jsx
@@ -1,20 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import progessionData from './data';
 import Curve from '../graphs/curve';
-import { PageHeader, Row, Col } from 'antd';
+import { PageHeader, Row, Col, Card } from 'antd';
 import { flag } from 'country-emoji';
+import { Progress } from 'antd';
 
+const fixKeys = {
+	China: 'Mainland China',
+};
+
+const useCountryData = countryId => {
+	const [data, setData] = useState({});
+	useEffect(() => {
+		fetch(`https://covid19.mathdro.id/api/countries/${countryId}`)
+			.then(cData => cData.json())
+			.then(({ confirmed, recovered, deaths }) =>
+				setData({
+					confirmed: parseInt(confirmed.value),
+					recovered: {
+						value: parseInt(recovered.value),
+						percent: Math.round(
+							(parseInt(recovered.value) / parseInt(confirmed.value)) * 100,
+						),
+					},
+					deaths: {
+						value: parseInt(deaths.value),
+						percent: Math.round(
+							(parseInt(deaths.value) / parseInt(confirmed.value)) * 100,
+						),
+					},
+				}),
+			);
+	}, [countryId]);
+
+	return [data];
+};
 const Country = ({ countryId }) => {
 	console.log({ countryId });
-	const countryData = progessionData[countryId];
+	const [data] = useCountryData(countryId);
+	const overrideKey = fixKeys[countryId] ? fixKeys[countryId] : countryId;
+	const countryData = progessionData[overrideKey];
+	console.log({ data });
 	return (
 		<div>
-			<Row justify="center">
-				<Col span={16}>
+			<Row justify="center" style={{ margin: '1rem' }}>
+				<Col align="center" span={16}>
 					<PageHeader title={`${flag(countryId)} ${countryId}`} />
 				</Col>
+			</Row>
+			<Row justify="center" style={{ margin: '1rem' }}>
+				<Col align="center" span={8}>
+					<Card title="Recovered" bordered={false}>
+						<Progress
+							strokeColor="green"
+							type="circle"
+							percent={data.recovered && data.recovered.percent}
+						/>
+					</Card>
+				</Col>
+				<Col align="center" span={8}>
+					<Card title="Deaths" bordered={false}>
+						<Progress
+							strokeColor="red"
+							type="circle"
+							percent={data.deaths && data.deaths.percent}
+						/>
+					</Card>
+				</Col>
+			</Row>
+			<Row justify="center" style={{ margin: '1rem' }}>
 				<Col span={16}>
-					<PageHeader title="Progression of inffected people day by day"/>
+					<PageHeader title="Progression of inffected people day by day" />
 					<Curve rawData={countryData} />
 				</Col>
 			</Row>
