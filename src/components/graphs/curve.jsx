40afd1dda2caeb9c12f38de804940f2593fe5219
Alan Campora <alan.campora@gmail.com>
2020-03-16 13:18:41 -0300

    refactor(country): add responsive chart

--- src/components/graphs/curve.jsx
+++ src/components/graphs/curve.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import map from 'lodash/map';
-/* 
+/*
  *rawData = {
  *  china: {
  *    'day-1': {
@@ -20,39 +20,41 @@ import {
 	CartesianGrid,
 	Tooltip,
 	Legend,
+	ResponsiveContainer,
 } from 'recharts';
 
 const Chart = ({ rawData }) => {
+	const formattedData = map(rawData, (value, key) => ({
+		name: key.replace(/-/g, ''),
+		cases: value.confirmed,
+	}));
 
-  const formattedData = map(rawData, (value, key) => ({
-    name: key.replace(/-/g,''),
-    cases: value.confirmed
-  }));
-
-  const values = map(rawData, (value, key) => value.confirmed);
-  const yDomain = [Math.min(...values), Math.max(...values)];
+	const values = map(rawData, (value, key) => value.confirmed);
+	const yDomain = [Math.min(...values), Math.max(...values)];
 
 	const data = formattedData;
 	return (
-		<LineChart
-			width={1000}
-			height={500}
-			data={data}
-			margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
-		>
-			<XAxis dataKey="name"/>
-			<YAxis domain={yDomain}/>
-			<CartesianGrid strokeDasharray="3 3" />
-			<Tooltip />
-			<Legend />
-			<Line
-				type="monotone"
-				dataKey="cases"
-				stroke="#8884d8"
-        strokeWidth="3"
-				activeDot={{ r: 8 }}
-			/>
-		</LineChart>
+		<ResponsiveContainer aspect={1.6}>
+			<LineChart
+				width={1000}
+				height={500}
+				data={data}
+				margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+			>
+				<XAxis dataKey="name" />
+				<YAxis domain={yDomain} />
+				<CartesianGrid strokeDasharray="3 3" />
+				<Tooltip />
+				<Legend />
+				<Line
+					type="monotone"
+					dataKey="cases"
+					stroke="#8884d8"
+					strokeWidth="3"
+					activeDot={{ r: 8 }}
+				/>
+			</LineChart>
+		</ResponsiveContainer>
 	);
 };
 
