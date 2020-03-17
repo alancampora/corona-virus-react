8893aebd4afff359d245c51d318ad950e7e2b23c
Alan Campora <alan.campora@gmail.com>
2020-03-17 19:18:40 -0300

    refactor(*): update to make it work with now

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import { StateProvider } from './context';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import {StateProvider} from './context';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './views/home';
 import Country from './views/country';
 
 function App() {
-	return (
-		<StateProvider>
-      <Router>
-				<Switch>
-					<Route exact path="/" component={Home} />
-          <Route exact path="/country/:countryId" component={Country} />
-				</Switch>
-			</Router>
-		</StateProvider>
-	);
+    return (
+        <StateProvider>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/country/:countryId" component={Country}/>
+                </Switch>
+            </Router>
+        </StateProvider>
+    );
 }
 
 export default App;
